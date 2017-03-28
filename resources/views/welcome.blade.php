04bec8a2d3443901af46789bb88280ef8de48d56
Mohamed Said <themohamedsaid@gmail.com>
2017-03-28 16:02:18 +0200

    build

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,8 @@
                     Laravel
                 </div>
 
+                123
+
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
