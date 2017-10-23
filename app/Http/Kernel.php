7f24d24886979cbd315ef2dba5de37bee328a790
Mohamed Said <themohamedsaid@gmail.com>
2017-10-23 21:22:19 +0200

    wip

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,7 +52,6 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'test' => TestMiddleware::class,
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,10 +2,7 @@
 
 namespace App\Providers;
 
-use App\Http\Middleware\TestMiddleware;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,9 +23,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        Passport::routes(null, [
-            'middleware' => TestMiddleware::class
-        ]);
     }
 }
